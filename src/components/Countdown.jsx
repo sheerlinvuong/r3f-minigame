cbee06fe6a4363addaab1bd065ee83907be95759
Sheerlin V <sheerlinv@Sheerlins-MacBook-Pro.local>
2026-08-11 16:27:02 +0100

    Style Countdown

---
+++ src/components/Countdown.jsx
@@ -0,0 +1,11 @@
+const Countdown = () => {
+  return (
+    <div className="countdown">
+      <img src="../assets/images/count-3.png" />
+      <img src="../assets/images/count-2.png" />
+      <img src="../assets/images/count-1.png" />
+    </div>
+  );
+};
+
+export default Countdown;
