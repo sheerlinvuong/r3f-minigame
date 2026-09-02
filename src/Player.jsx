2a2d6c025f750c3f7d46c72011a7586aa4541959
Sheerlin V <sheerlinv@Sheerlins-MacBook-Pro.local>
2026-09-02 15:04:02 +0100

    Add Character, animation, rotation and fix surfaces

--- src/Player.jsx
+++ src/Player.jsx
@@ -1,13 +1,29 @@
 import { CapsuleCollider, RigidBody } from "@react-three/rapier";
 import { useFrame } from "@react-three/fiber";
 import { useKeyboardControls } from "@react-three/drei";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import { Character } from "./components/Character";
+import { MathUtils } from "three";
+
+function lerpAngle(current, target, alpha) {
+  const difference = Math.atan2(
+    Math.sin(target - current),
+    Math.cos(target - current),
+  );
+
+  return current + difference * alpha;
+}
 
 export default function Player() {
   const body = useRef();
-  const [subscribeKeys, getKeys] = useKeyboardControls();
+  const [_, getKeys] = useKeyboardControls();
   const startPostion = [0, 2, 0];
   const speed = 4;
+
+  const container = useRef();
+  const rotationTarget = useRef(0);
+
+  const [animation, setAnimation] = useState("0028_OUJI"); //idle
 
   useFrame((state, delta) => {
     const { forward, backward, leftward, rightward } = getKeys();
@@ -33,29 +49,38 @@ export default function Player() {
         movement.x = -1;
       }
 
-      if (movement.z !== 0 || movement.x !== 0) {
+      if (movement.x !== 0 || movement.z !== 0) {
+        rotationTarget.current = Math.atan2(movement.x, movement.z);
+
         vel.x = speed * movement.x;
         vel.z = speed * movement.z;
+
+        setAnimation("0003_OUJI");
+      } else {
+        setAnimation("0028_OUJI");
       }
 
       body.current.setLinvel(vel, true);
     }
+
+    container.current.rotation.y = lerpAngle(
+      container.current.rotation.y,
+      rotationTarget.current,
+      0.1,
+    );
   });
 
   return (
-    <>
-      <RigidBody
-        ref={body}
-        colliders={false}
-        position={startPostion}
-        lockRotations
-      >
-        <mesh castShadow scale={0.5}>
-          <capsuleGeometry />
-          <meshStandardMaterial flatShading={true} color="orangered" />
-        </mesh>
+    <RigidBody
+      ref={body}
+      colliders={false}
+      position={startPostion}
+      lockRotations
+    >
+      <group ref={container}>
+        <Character position-y={-0.7} animation={animation} />
         <CapsuleCollider args={[0.25, 0.5]} />
-      </RigidBody>
-    </>
+      </group>
+    </RigidBody>
   );
 }
